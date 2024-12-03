794f2639fdb231cff4c119a8db75f32c4370bec2
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-12-03 21:19:55 +0100

    [02] rec: Try part 2 but overlapping symbols get counted multiple times

--- quest02/src/main.rs
+++ quest02/src/main.rs
@@ -3,14 +3,30 @@ use utils::input;
 fn main() {
     let input = input::read_file("inputs/part1.txt");
     println!("exercise 1: {}", part1(&input));
+    let input = input::read_file("inputs/part2.txt");
+    println!("exercise 2: {}", part2(&input));
 }
 
 fn part1(input: &String) -> usize {
     let mut res: usize = 0;
     let runes = get_runes(input);
+    let words = input.lines().last().unwrap();
 
     for rune in runes {
-        res += input.lines().last().unwrap().matches(&rune).count();
+        res += words.matches(&rune).count();
+    }
+    res
+}
+
+fn part2(input: &String) -> usize {
+    let mut res: usize = 0;
+    let runes = get_runes(input);
+    let words = input.lines().skip(2).collect::<String>();
+    let words_rev = words.chars().rev().collect::<String>();
+
+    for rune in runes {
+        res += words.matches(&rune).count() * rune.len();
+        res += words_rev.matches(&rune).count() * rune.len();
     }
     res
 }
@@ -35,5 +51,12 @@ mod test {
         let input = input::read_file("inputs/part1_example.txt");
         let res = part1(&input);
         assert_eq!(res, 12);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = input::read_file("inputs/part2_example.txt");
+        let res = part2(&input);
+        assert_eq!(res, 42);
     }
 }
