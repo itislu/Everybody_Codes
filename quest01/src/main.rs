7fbcce19abdfa2577be915b2dc0af0f75f4410e8
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-12-03 17:44:30 +0100

    [01] feat: Solve part 2

--- quest01/src/main.rs
+++ quest01/src/main.rs
@@ -1,16 +1,27 @@
 use utils::input;
 
 fn main() {
-    let input = input::read_input();
-    println!("exercise 1: {}", exercise1(&input));
-    // println!("exercise 2: {}", exercise2(&input));
+    let input = input::read_file("inputs/part1.txt");
+    println!("exercise 1: {}", part1(&input));
+    let input = input::read_file("inputs/part2.txt");
+    println!("exercise 2: {}", part2(&input));
 }
 
-fn exercise1(input: &String) -> usize {
+fn part1(input: &String) -> usize {
     count_occurrences(input, 'B') + count_occurrences(input, 'C') * 3
 }
 
-fn count_occurrences(input: &str, c: char) -> usize {
+fn part2(input: &String) -> usize {
+    let mut res: usize = part1(input) + count_occurrences(input, 'D') * 5;
+    for pair in input.as_bytes().chunks(2) {
+        if pair[0] as char != 'x' && pair[1] as char != 'x' {
+            res += 2;
+        }
+    }
+    res
+}
+
+fn count_occurrences(input: &String, c: char) -> usize {
     input.matches(c).count()
 }
 
@@ -19,16 +30,16 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_ex1() {
-        let input = input::read_example();
-        let res = exercise1(&input);
+    fn test_part1() {
+        let input = input::read_file("inputs/part1_example.txt");
+        let res = part1(&input);
         assert_eq!(res, 5);
     }
 
-    // #[test]
-    // fn test_ex2() {
-    //     let input = input::read_example();
-    //     let res = exercise2(&input);
-    //     assert_eq!(res, 48);
-    // }
+    #[test]
+    fn test_part2() {
+        let input = input::read_file("inputs/part2_example.txt");
+        let res = part2(&input);
+        assert_eq!(res, 28);
+    }
 }
