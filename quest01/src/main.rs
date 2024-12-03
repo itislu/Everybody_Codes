bf8713d2ff6d0f97fdd97434fc44b759809bb787
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-12-03 19:15:38 +0100

    [01] feat: Solve part 3

--- quest01/src/main.rs
+++ quest01/src/main.rs
@@ -5,6 +5,8 @@ fn main() {
     println!("exercise 1: {}", part1(&input));
     let input = input::read_file("inputs/part2.txt");
     println!("exercise 2: {}", part2(&input));
+    let input = input::read_file("inputs/part3.txt");
+    println!("exercise 3: {}", part3(&input));
 }
 
 fn part1(input: &String) -> usize {
@@ -14,8 +16,20 @@ fn part1(input: &String) -> usize {
 fn part2(input: &String) -> usize {
     let mut res: usize = part1(input) + count_occurrences(input, 'D') * 5;
     for pair in input.as_bytes().chunks(2) {
-        if pair[0] as char != 'x' && pair[1] as char != 'x' {
+        if pair[0] != b'x' && pair[1] != b'x' {
             res += 2;
+        }
+    }
+    res
+}
+
+fn part3(input: &String) -> usize {
+    let mut res: usize = part1(input) + count_occurrences(input, 'D') * 5;
+    for triple in input.as_bytes().chunks(3) {
+        match triple.iter().filter(|&&c| c == b'x').count() {
+            0 => res += 6,
+            1 => res += 2,
+            _ => {}
         }
     }
     res
@@ -41,5 +55,12 @@ mod test {
         let input = input::read_file("inputs/part2_example.txt");
         let res = part2(&input);
         assert_eq!(res, 28);
+    }
+
+    #[test]
+    fn test_part3() {
+        let input = input::read_file("inputs/part3_example.txt");
+        let res = part3(&input);
+        assert_eq!(res, 30);
     }
 }
