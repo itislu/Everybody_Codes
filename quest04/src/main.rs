b7867e0916efd216cfcf5a9cfb7e3b4ba396d254
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2025-04-18 21:06:53 +0000

    [04] feat: Solve part 2

--- quest04/src/main.rs
+++ quest04/src/main.rs
@@ -2,10 +2,12 @@ use utils::input;
 
 fn main() {
     let input = input::read_file("inputs/part1.txt");
-    println!("exercise 1: {}", part1(&input));
+    println!("exercise 1: {}", part1and2(&input));
+    let input = input::read_file("inputs/part2.txt");
+    println!("exercise 2: {}", part1and2(&input));
 }
 
-fn part1(input: &String) -> usize {
+fn part1and2(input: &String) -> usize {
     let nails: Vec<usize> = input.lines().map(|line| line.parse().unwrap()).collect();
     let min = nails.iter().min().unwrap_or(&0);
     nails.iter().map(|nail| nail - min).sum()
@@ -21,15 +23,26 @@ mod tests {
         #[test]
         fn example() {
             let input = input::read_file("inputs/part1_example.txt");
-            let res = part1(&input);
+            let res = part1and2(&input);
             assert_eq!(res, 10);
         }
 
         #[test]
         fn answer() {
             let input = input::read_file("inputs/part1.txt");
-            let res = part1(&input);
+            let res = part1and2(&input);
             assert_eq!(res, 84);
+        }
+    }
+
+    mod part2 {
+        use super::*;
+
+        #[test]
+        fn answer() {
+            let input = input::read_file("inputs/part2.txt");
+            let res = part1and2(&input);
+            assert_eq!(res, 919880);
         }
     }
 }
