db81534da5f562ed26e8443a1033a8a6d97c7528
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2025-04-19 22:00:53 +0000

    [05] refactor: Reuse code across all parts

--- quest05/src/main.rs
+++ quest05/src/main.rs
@@ -12,56 +12,20 @@ fn main() {
 
 fn part1(input: &String) -> String {
     let mut columns = parse_columns(input);
-    let col_count = columns.len();
 
-    for round in 0..10 {
-        let mut cur_col = round % col_count;
-        let clapper = columns[cur_col].pop_front().expect("empty column");
-        let mut claps_remaining = clapper;
-
-        loop {
-            cur_col = (cur_col + 1) % col_count;
-            if claps_remaining <= columns[cur_col].len() {
-                columns[cur_col].insert(claps_remaining - 1, clapper);
-                break;
-            }
-            if claps_remaining <= columns[cur_col].len() * 2 {
-                let idx = columns[cur_col].len() - (claps_remaining - columns[cur_col].len()) + 1;
-                columns[cur_col].insert(idx, clapper);
-                break;
-            }
-            claps_remaining -= columns[cur_col].len() * 2;
-        }
+    for round in 1..=10 {
+        do_round(&mut columns, round);
     }
-
     columns.iter().map(|col| col[0].to_string()).collect()
 }
 
 fn part2(input: &String) -> usize {
     let mut columns = parse_columns(input);
-    let col_count = columns.len();
     let mut round_results: HashMap<usize, usize> = HashMap::new();
     let mut round = 1;
 
     loop {
-        let old_col = (round - 1) % col_count;
-        let clapper = columns[old_col].pop_front().unwrap();
-        let new_col = (old_col + 1) % col_count;
-
-        let pos = (clapper - 1) % (columns[new_col].len() * 2);
-        let index = if pos < columns[new_col].len() {
-            pos
-        } else {
-            columns[new_col].len() - (pos - columns[new_col].len())
-        };
-        columns[new_col].insert(index, clapper);
-
-        let round_result: usize = columns
-            .iter()
-            .map(|col| col[0].to_string())
-            .collect::<String>()
-            .parse()
-            .unwrap();
+        let round_result = do_round(&mut columns, round);
         let new_count = round_results
             .get(&round_result)
             .and_then(|count| Some(count + 1))
@@ -78,33 +42,36 @@ fn part2(input: &String) -> usize {
 
 fn part3(input: &String) -> usize {
     let mut columns = parse_columns(input);
-    let col_count = columns.len();
     let elem_count = columns.iter().flatten().count();
     let mut round_results: HashSet<usize> = HashSet::new();
 
-    for round in 0..elem_count {
-        let old_col = round % col_count;
-        let clapper = columns[old_col].pop_front().unwrap();
-        let new_col = (old_col + 1) % col_count;
-
-        let pos = (clapper - 1) % (columns[new_col].len() * 2);
-        let index = if pos < columns[new_col].len() {
-            pos
-        } else {
-            columns[new_col].len() - (pos - columns[new_col].len())
-        };
-        columns[new_col].insert(index, clapper);
-
-        let round_result: usize = columns
-            .iter()
-            .map(|col| col[0].to_string())
-            .collect::<String>()
-            .parse()
-            .unwrap();
+    for round in 1..=elem_count {
+        let round_result = do_round(&mut columns, round);
         round_results.insert(round_result);
     }
-
     *round_results.iter().max().unwrap()
+}
+
+fn do_round(columns: &mut Vec<VecDeque<usize>>, round: usize) -> usize {
+    let col_count = columns.len();
+    let old_col = (round - 1) % col_count;
+    let clapper = columns[old_col].pop_front().unwrap();
+    let new_col = (old_col + 1) % col_count;
+
+    let pos = (clapper - 1) % (columns[new_col].len() * 2);
+    let index = if pos < columns[new_col].len() {
+        pos
+    } else {
+        columns[new_col].len() - (pos - columns[new_col].len())
+    };
+    columns[new_col].insert(index, clapper);
+
+    columns
+        .iter()
+        .map(|col| col[0].to_string())
+        .collect::<String>()
+        .parse()
+        .unwrap()
 }
 
 fn parse_columns(input: &String) -> Vec<VecDeque<usize>> {
