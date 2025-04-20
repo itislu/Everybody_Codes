d863fd59640ef2014981ba5c5da6acbdabf3d8fe
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2025-04-20 00:52:24 +0000

    [06] feat: Solve part 2

--- quest06/src/main.rs
+++ quest06/src/main.rs
@@ -4,6 +4,8 @@ use utils::input;
 fn main() {
     let input = input::read_file("inputs/part1.txt");
     println!("exercise 1: {}", part1(&input));
+    let input = input::read_file("inputs/part2.txt");
+    println!("exercise 2: {}", part2(&input));
 }
 
 fn part1(input: &String) -> String {
@@ -21,8 +23,30 @@ fn part1(input: &String) -> String {
     path_to_string(paths.values().filter_map(|v| v.as_ref()).next().unwrap())
 }
 
+fn part2(input: &String) -> String {
+    let tree = Tree::new(input);
+    let mut paths: HashMap</*length*/ usize, /*path*/ Option<Vec<Node>>> = HashMap::new();
+
+    for fruit in tree.fruits() {
+        let path: Vec<Node> = tree.path(fruit);
+        paths
+            .entry(path.len())
+            .and_modify(|e| *e = None)
+            .or_insert(Some(path));
+    }
+
+    path_to_string_short(paths.values().filter_map(|v| v.as_ref()).next().unwrap())
+}
+
 fn path_to_string(path: &Vec<Node>) -> String {
     path.iter().rev().map(|node| node.to_string()).collect()
+}
+
+fn path_to_string_short(path: &Vec<Node>) -> String {
+    path.iter()
+        .rev()
+        .map(|node| node.to_string().chars().next().unwrap())
+        .collect()
 }
 
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
@@ -101,6 +125,17 @@ mod tests {
             let input = input::read_file("inputs/part1.txt");
             let res = part1(&input);
             assert_eq!(res, "RRBSDGVPJHKG@");
+        }
+    }
+
+    mod part2 {
+        use super::*;
+
+        #[test]
+        fn answer() {
+            let input = input::read_file("inputs/part2.txt");
+            let res = part2(&input);
+            assert_eq!(res, "RFBMNWSHLW@");
         }
     }
 }
