f4e3ec791351da717d44bf2ba5c5a2121c8bb460
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2025-06-01 15:04:24 +0000

    [09] feat: Solve part 3

--- quest09/src/main.rs
+++ quest09/src/main.rs
@@ -6,6 +6,8 @@ fn main() {
     println!("exercise 1: {}", part1(&input));
     let input = input::read_file("inputs/part2.txt");
     println!("exercise 2: {}", part2(&input));
+    let input = input::read_file("inputs/part3.txt");
+    println!("exercise 3: {}", part3(&input));
 }
 
 // Greedy
@@ -44,6 +46,36 @@ fn part2(input: &str) -> usize {
     for target in targets {
         total += min_stamps(&stamps, target, &mut solutions)
             .expect(format!("no solution for {target}").as_str());
+    }
+    total
+}
+
+fn part3(input: &str) -> usize {
+    let targets: Vec<usize> = parse::numbers(input).collect();
+    let mut stamps = vec![
+        1, 3, 5, 10, 15, 16, 20, 24, 25, 30, 37, 38, 49, 50, 74, 75, 100, 101,
+    ];
+    stamps.sort_by(|a, b| b.cmp(a)); // sort descending to reduce recursion in first go
+    let stamps = stamps; // make immutable
+    let max_diff = 100;
+    let mut total = 0;
+    let mut solutions: Vec<Option<usize>> = vec![None; *targets.iter().max().unwrap() + 1];
+
+    for target in targets {
+        let mut min: Option<usize> = None;
+        let mut t1 = target / 2;
+        let mut t2 = t1 + target % 2;
+
+        while t2 - t1 <= max_diff {
+            if let Some(sum) = min_stamps(&stamps, t1, &mut solutions)
+                .and_then(|sol1| min_stamps(&stamps, t2, &mut solutions).map(|sol2| sol1 + sol2))
+            {
+                min = Some(sum.min(min.unwrap_or(sum)));
+            }
+            t1 -= 1;
+            t2 += 1;
+        }
+        total += min.expect(format!("no solution for {target}").as_str());
     }
     total
 }
@@ -101,6 +133,24 @@ mod tests {
             let input = input::read_file("inputs/part2.txt");
             let res = part2(&input);
             assert_eq!(res, 5057);
+        }
+    }
+
+    mod part3 {
+        use super::*;
+
+        #[test]
+        fn example() {
+            let input = input::read_file("inputs/part3_example.txt");
+            let res = part3(&input);
+            assert_eq!(res, 10449);
+        }
+
+        #[test]
+        fn answer() {
+            let input = input::read_file("inputs/part3.txt");
+            let res = part3(&input);
+            assert_eq!(res, 148836);
         }
     }
 }
